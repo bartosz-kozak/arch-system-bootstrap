cc181363a6d3347fd1b3b3b13ac2daa877bf4e4c
bartosz-kozak <konas13@gmail.com>
2026-04-05 23:24:10 +0200

    Poprawiony instalator, kolejna poprawa instalatora miniforge.

--- install.sh
+++ install.sh
@@ -237,8 +237,10 @@ install_miniforge() {
     die "Pobrany plik instalatora Miniforge3 nie wygląda poprawnie: ${installer_path}"
   fi
 
+  run_as_target_user chmod +x "${installer_path}"
+
   log "Instaluję Miniforge3 w ${miniforge_dir}."
-  run_as_target_user bash "${installer_path}" -b -p "${miniforge_dir}"
+  run_as_target_user "${installer_path}" -b -p "${miniforge_dir}"
   run_as_target_user rm -f -- "${installer_path}"
 }
 
